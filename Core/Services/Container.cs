ed4e26af207797ee482f8acf3a0fb6e0ab535aef
Christoffer Reijer <ephracis@gmail.com>
2016-05-28 09:14:49 +0200

    Add container for managing services and interfaces
    The Container class exposes a Unity Container for IoC, allowing
    the bootstrapper to register instances and interfaces, and all other
    code to resolve their dependencies.
    
    This adds a dependency on the Nuget package Unity.

---
+++ Core/Services/Container.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace Stoffi.Core.Services
+{
+    /// <summary>
+    /// Used for locating an instance of the IoC container.
+    /// </summary>
+    public class Container
+    {
+        public static IUnityContainer Instance { get; private set; }
+        static Container()
+        {
+            Instance = Instance ?? new UnityContainer();
+        }
+    }
+}
